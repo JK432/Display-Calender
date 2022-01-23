16ce059cdeb67f053880129aa09d62c5d3aee1a8
JK4302 <72574346+JK432@users.noreply.github.com>
2022-01-23 03:31:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# Write a Python Program to display the calender by using functions
+import calendar  
+
+yy = int(input("Enter year: "))  
+mm = int(input("Enter month: "))  
+  
+  
+print(calendar.month(yy,mm))  
